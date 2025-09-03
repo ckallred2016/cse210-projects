d08d0f63baa4069a8f9ad9b15e36b869bf235edc
ckallred2016 <ckallred2016@gmail.com>
2025-09-03 13:50:48 -0600

    Finished and compared my solution to the actual solution. Looking good!

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+
+        int x = int.Parse(gradePercentage);
+        string letter = "";
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "d";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Yay! You passed the class!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry! Remember what you've learned and try again! You've got this!");
+        }
     }
 }
