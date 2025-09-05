469fc3a653c6a0db5215bae7af6b94ee6ebea05e
ckallred2016 <ckallred2016@gmail.com>
2025-09-05 15:32:39 -0600

    #1 done for the #3 asignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,22 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        string response = "yes";
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
 
-        while (response == "yes")
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (magicNumber < guess)
         {
-            Console.Write("Would you like to play again? ");
-            response = Console.ReadLine();
+            Console.WriteLine("Lower");
         }
 
-        Random randomGenerator = new Random();
-        int secretNumber = randomGenerator.Next(1, 11);
-
-        do
+        else if (magicNumber > guess)
         {
-            Console.Write("What is the magic number? ")
-            secretNumber = Console.ReadLine();
-        } while (secretNumber == 0);
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+        // string response = "yes";
+
+        // while (response == "yes")
+        // {
+        //     Console.Write("Would you like to play again? ");
+        //     response = Console.ReadLine();
+        // }
+
+        // Random randomGenerator = new Random();
+        // int secretNumber = randomGenerator.Next(1, 11);
+
+        // do
+        // {
+        //     Console.Write("What is the magic number? ")
+        //     secretNumber = Console.ReadLine();
+        // } while (secretNumber == 0);
 
 
     }
