45bfa227f4ec1836a99fd56c1f27d7f5d7c26e71
ckallred2016 <ckallred2016@gmail.com>
2025-09-04 20:35:51 -0600

    I know I not understanding, this is what I have so far...

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Console.Write("Would you like to play again? ");
+            response = Console.ReadLine();
+        }
+
+        Random randomGenerator = new Random();
+        int secretNumber = randomGenerator.Next(1, 11);
+
+        do
+        {
+            Console.Write("What is the magic number? ")
+            secretNumber = Console.ReadLine();
+        } while (secretNumber == 0);
+
+
     }
 }
