4e337f5bddb2bb6857a07b80c52e08810441922a
ckallred2016 <ckallred2016@gmail.com>
2025-09-06 12:05:08 -0600

    It's a little different than the sample solution but it shows the same results! Hopefully I did it in an okay way!

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayResult();
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static string PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        return userInput;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult()
+    {
+        // Display welcome message
+        DisplayWelcome();
+
+        // Prompts for user name
+        string userName = PromptUserName();
+
+        // PromptUserName for user's favorite number, then converts it from a sting to an int
+        string userInput = PromptUserNumber();
+        int number = int.Parse(userInput);
+        // Squares the user's favorite number
+        int result = SquareNumber(number);
+
+        Console.WriteLine($"{userName}, the square of your number is {result}");
+    }
+    
 }
